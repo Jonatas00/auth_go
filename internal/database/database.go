6ed29e4a315144110e154f68d865a2d20e4c7984
Jonatas <85264333+Jonatas00@users.noreply.github.com>
2025-02-27 22:54:24 -0300

    fix: remove print

--- internal/database/database.go
+++ internal/database/database.go
@@ -15,7 +15,6 @@ func Connect() {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
 		config.DB_HOST, config.DB_USER, config.DB_PASSWORD, config.DB_NAME, config.DB_PORT,
 	)
-	fmt.Println(dsn)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		TranslateError: true,
 	})
